de4a519ea6dd9042ce5c003445b2416ab0cf4315
SAHIL SHARMA <118820911+sahildando@users.noreply.github.com>
2024-10-13 00:21:58 +0530

    Time: 0 ms (100%), Space: 8.9 MB (5.04%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack <char> st;
+        for (char & ch:s){
+            if (ch == '(')
+             st.push(')');
+
+             else  if (ch == '{')
+             st.push('}');
+
+             else  if (ch == '[')
+             st.push(']');
+
+             else if (st.empty() || st.top() != ch){
+                return false;
+             } else {
+                st.pop();
+             }
+        }
+        return st.empty();
+    }
+};
