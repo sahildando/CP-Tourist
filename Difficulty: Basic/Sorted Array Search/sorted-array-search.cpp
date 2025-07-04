72f322a9d4efe00b35f472ada7237fd8fbeca430
Mark Grayson <118820911+sahildando@users.noreply.github.com>
2025-07-04 21:30:06 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Sorted Array Search/sorted-array-search.cpp
@@ -0,0 +1,11 @@
+class Solution {
+  public:
+    // Function to find element in sorted array
+    // arr: input array
+    // k: element to be searched
+    bool searchInSorted(vector<int>& arr, int k) {
+
+        // Your code here
+        return binary_search(arr.begin(),arr.end(),k);
+    }
+};
