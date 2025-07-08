89f1144eb3fe272a13c25e9cc168a02aedcf7088
Mark Grayson <118820911+sahildando@users.noreply.github.com>
2025-07-08 17:59:31 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Check Equal Arrays/check-equal-arrays.cpp
+++ Difficulty: Easy/Check Equal Arrays/check-equal-arrays.cpp
@@ -1,11 +1,3 @@
-//{ Driver Code Starts
-// Initial template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 // User function template for C++
 
 class Solution {
@@ -13,53 +5,14 @@ class Solution {
     // Function to check if two arrays are equal or not.
     bool checkEqual(vector<int>& a, vector<int>& b) {
         // code here
-           int n = a.size(), m = b.size();
-    if (n != m)
-        return false;
-
-    unordered_map<int, int> mp;
-    for (int i = 0; i < n; i++)
-        mp[a[i]]++;
-
-    for (int i = 0; i < n; i++) {
-        if (mp.find(b[i]) == mp.end())
-            return false;
-
-        if (mp[b[i]] == 0)
-            return false;
-      
-        mp[b[i]]--;
-    }
-    return true;
+       sort(a.begin(),a.end());
+       sort(b.begin(),b.end());
+       
+       if (a.size() != b.size())
+       return 0;
+       if (a == b)
+       return 1;
+       else
+       return 0;
     }
 };
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore(); // Ignore the newline character after t
-    while (t--) {
-        vector<int> arr1, arr2;
-        string input1, input2;
-
-        getline(cin, input1); // Read the entire line for the first array elements
-        stringstream ss1(input1);
-        int number;
-        while (ss1 >> number) {
-            arr1.push_back(number);
-        }
-
-        getline(cin, input2); // Read the entire line for the second array elements
-        stringstream ss2(input2);
-        while (ss2 >> number) {
-            arr2.push_back(number);
-        }
-
-        Solution ob;
-        cout << (ob.checkEqual(arr1, arr2) ? "true" : "false") << "\n~\n";
-    }
-    return 0;
-}
-// } Driver Code Ends
