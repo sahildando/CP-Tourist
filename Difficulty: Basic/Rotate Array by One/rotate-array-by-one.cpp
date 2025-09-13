ea433aca3e0c4043c62c37dafab91557d2a81164
Mark Grayson <118820911+sahildando@users.noreply.github.com>
2025-09-13 22:42:26 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Rotate Array by One/rotate-array-by-one.cpp
@@ -0,0 +1,11 @@
+// User function Template for C++
+
+class Solution {
+  public:
+    void rotate(vector<int> &arr) {
+        // code here
+        int n = arr.size();
+        reverse(arr.begin(), arr.end());
+        reverse(arr.begin()+1, arr.end());
+    }
+};
